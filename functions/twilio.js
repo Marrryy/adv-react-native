96ef32bd18dde094a4949115cbe57dd2315e6b83
Marrryy <ling.310500@gmail.com>
2019-08-09 10:43:19 +0700

    deleting token

--- functions/twilio.js
+++
@@ -1,6 +0,0 @@
-const twilio = require('twilio');
-
-const accountSid ='AC0a127ca4691f114de098534e9e52a226';
-const authToken ='ae81a868789f5cc5d69ba0ddce54eac1';
-
-module.exports = new twilio.Twilio(accountSid,authToken);
